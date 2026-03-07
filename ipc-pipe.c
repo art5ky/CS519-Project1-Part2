c388a34ef07803296264b930530e8255f709d085
Arthur Levitsky <artsky.task@gmail.com>
2026-03-07 00:31:35 -0500

    added some variables for handling how to divide matrix rows and their remainders

--- ipc-pipe.c
+++ ipc-pipe.c
@@ -14,24 +14,31 @@
 #include "headers/benchmark.h"
 #include "headers/locks.h"
 
-#define SIZE 1000
-#define RANGE 999
+#define MATRIX_SIZE 1000
+#define ENTRY_RANGE 999
 
 int main() {
+
     srand(time(NULL));
     struct timespec start, end; 
-    
-    int **A = malloc_sq_matrix(SIZE);
-    int **B = malloc_sq_matrix(SIZE);
-    int **C = malloc_sq_matrix(SIZE);
+    long workers = sysconf(_SC_NPROCESSORS_ONLN);
+    int base_rows = MATRIX_SIZE / workers; 
+    int remainder = MATRIX_SIZE % workers; 
+
+    printf("%d\n", base_rows);
+    printf("%d\n", remainder);
+
+    int **A = malloc_sq_matrix(MATRIX_SIZE);
+    int **B = malloc_sq_matrix(MATRIX_SIZE);
+    int **C = malloc_sq_matrix(MATRIX_SIZE);
 
     if (A == NULL || B == NULL || C == NULL) {
         fprintf(stderr, "Allocation for matrices failed.");
         return 1; 
     }
     
-    rand_init_sq_matrix(A, RANGE);
-    rand_init_sq_matrix(B, RANGE);
+    rand_init_sq_matrix(A, ENTRY_RANGE);
+    rand_init_sq_matrix(B, ENTRY_RANGE);
     rand_init_sq_matrix(C, 1);
 
     clock_gettime(CLOCK_MONOTONIC, &start);
@@ -40,7 +47,7 @@ int main() {
 
     double total_time_sec = get_total_time(start, end);
    
-    print_stats(SIZE, 1, true, total_time_sec);
+    print_stats(MATRIX_SIZE, workers, true, total_time_sec);
 
     free_sq_matrix(A);
     free_sq_matrix(B);
